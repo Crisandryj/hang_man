630c7802fed87bc4270104aa09216ad07586c0ea
Crisandry Javier <crisandryj@gmail.com>
2022-04-28 11:13:38 -0400

    Display guess word

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,15 +1,26 @@
+def display_guess(guessword)
+    player_guess = []
+    count = 0
+    until count == guessword.length
+    player_guess.push('_')
+    count +=1
+    end
+    p player_guess  
+end 
+
+
 contents = File.readlines('google-10000-english-no-swears.txt')
 
 mid_size_words = contents.select{|word| word.length.between?(5,12)}
 
-p mid_size_words[rand(0..8449)]
-
 #create array with rand word generated
 #create length of guess based on size of guess word
 
+guessword = mid_size_words[rand(0..8449)].chop.split('')
 
+p guessword
 
-
+display_guess(guessword)
 
 #let player guess word
 #if letter doesn't exist in word, count strikes(3 and its over)
