7cd45ac26d85af64bf33d72bfb4e9887b91281df
Crisandry Javier <crisandryj@gmail.com>
2022-04-28 21:07:13 -0400

    hangman game ends after 3 strikes

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -12,37 +12,51 @@
       @Letter
       @player_guess = []
       @guessword = @@mid_size_words[rand(0..8449)].chop.split('')
-    end 
+      @count = 0
+    end
   
 
-  def display_guess()
+  def generate_guess_display()
     count = 0
     until count == @guessword.length
     @player_guess.push('_')
     count +=1
     end
-    p @player_guess  
   end 
 
   def gameover?(count)
-    if count == 3
+    if @count == 3
       p "GAMEOVER"
     end 
   end 
 
-  def hangman()
+  def play()
+    generate_guess_display
+
+    until @count == 3
     p "Guess Letter:"
     @letter = gets.chomp.downcase
+
+    p @guessword
+
+    if !@guessword.include?(@letter)
+      @count +=1
+    end 
+
     @guessword.each_with_index do |l,index|  
       if l == @letter
         @player_guess[index] = l
-        display_guess(@guessword,@player_guess)
+      else 
+        gameover?(@count)
       end 
     end 
+    p @player_guess
+    end 
+
   end 
 
 end 
 
 
 game = Hangman.new(Player)
-p game.guessword
+game.play
